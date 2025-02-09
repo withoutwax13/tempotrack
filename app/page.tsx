d3dbd1a8d333bcab3dc8af168a87964e8d67d75e
John Patrick Valera <johnpatrickyusoresvalera@gmail.com>
2025-02-09 16:34:48 +0800

    fix: update Home component to use next/navigation for routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 const Home = () => {
   const router = useRouter();
